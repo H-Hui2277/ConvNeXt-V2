ebe694b910c74b255242a84aca0801d2d5b7b98c
H-Hui2277 <mrharry@hnu.edu.cn>
2023-04-17 18:32:01 +0800

    Update single_validate.py
    Fix bugs.

--- single_validate.py
+++ single_validate.py
@@ -3,7 +3,7 @@ from torch.utils import model_zoo
 from torch.utils.data import DataLoader, SequentialSampler
 
 from datasets import build_dataset
-from models.convnextv2 import convnextv2_pico
+from models.convnextv2 import convnextv2_pico, convnextv2_tiny
 
 from engine_finetune import evaluate
 
@@ -19,30 +19,35 @@ model_urls = {
 }
 
 # Dataset
-args = object()
-args.data_set = 'IMNET'
-args.data_path = '' # path to ImgetNet
+class Args(object):
+    def __init__(self) -> None:
+        self.data_set = 'IMNET'
+        self.data_path = 'H:\\Datasets\\ImageNet\\imagenet' # path to ImgetNet
+        self.input_size = 224
+        self.imagenet_default_mean_and_std = True
+        self.crop_pct = None
+args = Args()
 
+if __name__ == '__main__':
+    # dataset_train, args.nb_classes = build_dataset(is_train=True, args=args)
+    dataset_val, _ = build_dataset(is_train=False, args=args)
+    sampler_val = SequentialSampler(dataset_val)
+    data_loader_val = DataLoader(
+        dataset_val, sampler=sampler_val,
+        batch_size=8,
+        num_workers=8,
+        pin_memory=True,
+        drop_last=False
+    )
 
-dataset_train, args.nb_classes = build_dataset(is_train=True, args=args)
-dataset_val, _ = build_dataset(is_train=False, args=args)
-sampler_val = SequentialSampler(dataset_val)
-data_loader_val = DataLoader(
-    dataset_val, sampler=sampler_val,
-    batch_size=4,
-    num_workers=8,
-    pin_memory=True,
-    drop_last=False
-)
+    # Model
+    device = torch.device('cuda:0')
+    model = convnextv2_tiny()
+    checkpoint = model_zoo.load_url(model_urls['tiny'], map_location='cpu')
+    model.load_state_dict(checkpoint['model'])
+    model.to(device)
 
-# Model
-device = torch.device('cuda:0')
-model = convnextv2_pico()
-checkpoint = model_zoo.load_url(model_urls['pico'], map_location='cpu')
-model.load_state_dict(checkpoint['model'])
-model.to(device)
-
-# Evaluate
-test_res = evaluate(data_loader_val, model, 'cuda:0')
-print(f"Accuracy of the network on {len(dataset_val)} test images: {test_res['acc1']:.5f}%")
+    # Evaluate
+    test_res = evaluate(data_loader_val, model, 'cuda:0')
+    print(f"Accuracy of the network on {len(dataset_val)} test images: {test_res['acc1']:.5f}%")
 
